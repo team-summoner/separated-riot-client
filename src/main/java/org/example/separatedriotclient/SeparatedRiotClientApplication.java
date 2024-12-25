2c07ce936e3fbc899966245172522d068b676d44
yokxim <yooksi53@gmail.com>
2024-12-25 16:06:15 +0900

    initial commit

---
+++ src/main/java/org/example/separatedriotclient/SeparatedRiotClientApplication.java
@@ -0,0 +1,13 @@
+package org.example.separatedriotclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeparatedRiotClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeparatedRiotClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/separatedriotclient/SeparatedRiotClientApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.separatedriotclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeparatedRiotClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
